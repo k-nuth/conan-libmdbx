815a4bfcfdbf9a791e737ef23cdf65e3c0eaa7ad
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-04-10 16:36:36 -0300

    first test

---
+++ .travis/install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+set -x
+
+if [[ "$(uname -s)" == 'Darwin' ]]; then
+    brew update || brew update
+    brew outdated pyenv || brew upgrade pyenv
+    brew install pyenv-virtualenv
+    brew install cmake || true
+
+    if which pyenv > /dev/null; then
+        eval "$(pyenv init -)"
+    fi
+
+    pyenv install 3.7.1
+    pyenv virtualenv 3.7.1 conan
+    pyenv rehash
+    pyenv activate conan
+fi
+
+pip install conan_package_tools==0.30.4 > /dev/null
+# pip install kthbuild==0.0.14 > /dev/null
+pip install kthbuild --upgrade > /dev/null 
+
+
+#conan remote add conan_bzip2 https://api.bintray.com/conan/k-nuth/kth
+
+conan user
---
+++ conanfile.py
@@ -0,0 +1,112 @@
+# Copyright (c) 2016-2020 Knuth Project developers.
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+from conans import ConanFile, CMake, tools
+import sys, os
+import fileinput
+
+def option_on_off(option):
+    return "ON" if option else "OFF"
+
+class LibMDBXConan(ConanFile):
+    name = "libmdbx"
+    description = "Fast embeddable key-value ACID database without WAL"
+    version = "0.7.0"
+    settings = "os", "compiler", "build_type", "arch"
+    url = "https://github.com/k-nuth/conan-libmdbx"
+    license = "OpenLDAP Public License"
+
+    generators = "cmake"
+
+    options = {"shared": [True, False],
+               "fPIC": [True, False],
+               "verbose": [True, False],
+               "include_bins": [True, False],
+    }
+
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+        "verbose": False,
+        "include_bins": False,
+    }
+
+    # exports = "conanfile.py", "mdb.def", "win32/*", "LICENSE.md"    # "CMakeLists.txt",
+    # exports_sources = ["CMakeLists.txt"]
+    build_policy = "missing"
+
+    @property
+    def msvc_mt_build(self):
+        return "MT" in str(self.settings.get_safe("compiler.runtime"))
+
+    @property
+    def fPIC_enabled(self):
+        if self.settings.compiler == "Visual Studio":
+            return False
+        else:
+            return self.options.fPIC
+
+    @property
+    def is_shared(self):
+        if self.options.shared and self.msvc_mt_build:
+            return False
+        else:
+            return self.options.shared
+
+    def config_options(self):
+        if self.settings.compiler == "Visual Studio":
+            self.options.remove("fPIC")
+            if self.options.shared and self.msvc_mt_build:
+                self.options.remove("shared")
+
+    def configure(self):
+        del self.settings.compiler.libcxx #Pure-C 
+
+    def package_id(self):
+        self.info.options.verbose = "ANY"
+
+    def source(self):
+        git = tools.Git()
+        git.clone("https://github.com/erthink/libmdbx.git")
+        git.checkout("v%s" % (self.version,))
+
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.verbose = self.options.verbose
+        cmake.definitions["BUILD_SHARED_LIBS"] = option_on_off(self.is_shared)
+        cmake.definitions["CMAKE_POSITION_INDEPENDENT_CODE"] = option_on_off(self.fPIC_enabled)
+        cmake.configure(source_dir=self.source_folder)
+        cmake.build()
+
+    def package(self):
+        self.copy("mdbx.h", dst="include")
+
+        if not self.options.shared:
+            self.copy("*.lib", dst="lib", keep_path=True)
+            self.copy("*.a", dst="lib", keep_path=True)
+
+        if self.options.shared:
+            self.copy("*.dll", dst="lib", keep_path=True)
+            self.copy("*.so", dst="lib", keep_path=True)
+            self.copy("*.dylib", dst="lib", keep_path=True)
+
+        if self.options.include_bins:
+            self.copy("mdbx_chk*", dst="bin", keep_path=True)
+            self.copy("mdbx_dump*", dst="bin", keep_path=True)
+            self.copy("mdbx_load*", dst="bin", keep_path=True)
+            self.copy("mdbx_stat*", dst="bin", keep_path=True)
+
+        self.copy("*.pdb", dst="lib", keep_path=True)
+
+    def package_info(self):
+        if self.settings.build_type == "Debug":
+            self.cpp_info.libs = ["libmdbx"]
+        else:
+            self.cpp_info.libs = ["libmdbx"]
+            
+        if  self.settings.os == "Windows":
+            self.cpp_info.libs.append("ntdll")
+        else:
+            self.cpp_info.libs.append("pthread")
