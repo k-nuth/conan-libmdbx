353607514349859fdc9acd20e4f1eea5aa74c4f9
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-04-10 16:54:25 -0300

    avoid building 32bit binaries

--- build.py
+++ build.py
@@ -1,7 +1,8 @@
 from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(username="kth", channel="stable")
+    builder = ConanMultiPackager(username="kth", channel="stable", archs=["x86_64"]
+                               , remotes="https://api.bintray.com/conan/k-nuth/kth")
     builder.add_common_builds(shared_option_name="libmdbx:shared", pure_c=True)
     builder.run()
 
